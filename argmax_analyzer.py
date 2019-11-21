0cef42c3c96284898271299f46b953aaa779d3ce
TobiasHuber <tobiashuber94@gmail.com>
2019-11-21 12:39:42 +0100

    argmax installer not necessary anymore but can be used to update

---
+++ argmax_analyzer.py
@@ -0,0 +1,130 @@
+from innvestigate import analyzer
+
+import keras
+import keras.backend as K
+import tensorflow as tf
+from innvestigate.analyzer.relevance_based.relevance_rule import Alpha1Beta0IgnoreBiasRule
+from innvestigate.analyzer.relevance_based.relevance_analyzer import _LRPFixedParams
+import innvestigate
+import custom_layers
+
+
+class Argmax(_LRPFixedParams):
+    """LRP-analyzer that uses the argmax rule"""
+
+    def __init__(self, model, *args, **kwargs):
+        innvestigate.analyzer.relevance_based.relevance_analyzer.LRP_RULES['Argmax'] = ArgmaxRule
+        super(Argmax, self).__init__(model, *args, rule="Argmax", **kwargs)
+
+
+class ArgmaxRule(Alpha1Beta0IgnoreBiasRule):
+    '''
+        implementation of the ArgmaxRule described in https://link.springer.com/chapter/10.1007/978-3-030-30179-8_16
+    '''
+    def __init__(self, *args, **kwargs):
+        layer = args[0]
+        self.layer = layer
+
+        # use classical LRP rule for non-convolutional layers and first convolutional layer after the input
+        if layer.__class__ == keras.layers.convolutional.Conv2D:
+            if layer._inbound_nodes[0].inbound_layers[0].__class__ == keras.engine.input_layer.InputLayer:
+                super(Alpha1Beta0IgnoreBiasRule, self).__init__(*args,
+                                                                alpha=1,
+                                                                beta=0,
+                                                                bias=False,
+                                                                **kwargs)
+            else:
+                self.weights = layer.weights[0]
+                self.padding = layer.padding
+                self.filter_size = layer.kernel_size[0]
+                #TODO assumes quadratic stride for now
+                self.stride = layer.strides[0]
+        else:
+            super(Alpha1Beta0IgnoreBiasRule, self).__init__(*args,
+                                                                     alpha=1,
+                                                                     beta=0,
+                                                                     bias=False,
+                                                                     **kwargs)
+
+
+    def apply(self, Xs, Ys, Rs, reverse_state):
+        # use classical LRP rule for non-convolutional layers and first convolutional layer after the input
+        if self.layer.__class__ != keras.layers.convolutional.Conv2D:
+            return super(Alpha1Beta0IgnoreBiasRule, self).apply(Xs,Ys,Rs,reverse_state)
+        if self.layer._inbound_nodes[0].inbound_layers[0].__class__ == keras.engine.input_layer.InputLayer:
+            return super(Alpha1Beta0IgnoreBiasRule, self).apply(Xs, Ys, Rs, reverse_state)
+
+        #remove additional array from innvestigate LRP stuff
+        #TODO this could break something
+        Xs = Xs[0] #TODO make this more generall
+        Rs = Rs[0]
+
+        weights = self.weights
+        padding = self.padding
+        filter_size = self.filter_size
+        stride = self.stride
+        input_shape = Xs.get_shape().as_list()[1:]
+
+        # getting the output of the previous layer and add padding if it was used while training the net
+        if (padding != 0):
+            if (padding == "valid"):
+                #TODO non quadratic filters
+                filter_height = filter_size
+                filter_width = filter_size
+
+                # valid padding doesnt really pad but only considers the values outside the original input as zero, so we just pad enough zeros at the ends of the input
+                pad_top = 0
+                pad_bottom = filter_height
+                pad_left = 0
+                pad_right = filter_size
+
+                # padded_input
+                out = K.spatial_2d_padding(Xs, padding=((pad_top, pad_bottom), (pad_left, pad_right)))
+
+            elif (padding == "same"):
+                in_height = input_shape[0]
+                in_width = input_shape[1]
+                filter_height = filter_size
+                filter_width = filter_size
+                strides = (1, stride, stride, 1)
+
+                # calculate padding according to https://www.tensorflow.org/api_guides/python/nn#Convolution ( tf version 1.11 )
+                if (in_height % strides[1] == 0):
+                    pad_along_height = max(filter_height - strides[1], 0)
+                else:
+                    pad_along_height = max(filter_height - (in_height % strides[1]), 0)
+                if (in_width % strides[2] == 0):
+                    pad_along_width = max(filter_width - strides[2], 0)
+                else:
+                    pad_along_width = max(filter_width - (in_width % strides[2]), 0)
+
+                pad_top = pad_along_height // 2
+                pad_bottom = pad_along_height - pad_top
+                pad_left = pad_along_width // 2
+                pad_right = pad_along_width - pad_left
+
+                #padded_input
+                out = K.spatial_2d_padding(Xs, padding=((pad_top, pad_bottom), (pad_left, pad_right)))
+
+            else:
+                raise ValueError('as of now only *same*,*valid* and *0* are supported paddings')
+
+            self.input_vector_length = 1
+            shape = list(out.shape)
+            shape.pop(0)
+            for i in shape:
+                self.input_vector_length *= i
+
+        # the actual argmax part
+        new_relevance_array = custom_layers.ArgmaxPositions(stride, filter_size, out, weights)(Rs)
+
+        #remove padding, if it was added
+        if (padding != 0):
+            new_relevance_array =  keras.layers.Lambda( lambda x: x[:,pad_top:-pad_bottom, pad_left:-pad_right, :])(new_relevance_array)
+
+        return new_relevance_array
+
+
+
+
+
---
+++ custom_layers.py
@@ -0,0 +1,116 @@
+import keras
+import keras.backend as K
+import tensorflow as tf
+
+class ArgmaxPositions(keras.layers.Layer):
+    ''' implementation of the LRP Argmax-Rule (https://link.springer.com/chapter/10.1007/978-3-030-30179-8_16)
+        for convolutional layers
+
+        Attributes
+        ----------
+        layer_output: the output of the layer to be analyzed
+        layer_weights: the weights of the layer to be analyzed
+        stride: the stride of the layer to be analyzed (has to be quadratic TODO make it free)
+        filter_size: the filter size of the layer to be analyzed (has to be quadratic TODO make it free)
+        relevance_values: the relevance values of the layer succeeding the layer to be analyzed
+
+
+        Methods
+        -------
+        update_relevance:
+            finds the most contributing position for a given relevance value and propagates the relevance to this position
+        call:
+            caluclates the relvance vlalues of the analyzed layer according to the argmax-rule.
+            Assumes no padding.
+     '''
+
+    def __init__(self, stride, filter_size, layer_output, layer_weights, **kwargs):
+        '''
+        :param layer_output: the output of the layer to be analyzed
+        :param layer_weights: the weights of the layer to be analyzed
+        :param stride: the stride of the layer to be analyzed (has to be quadratic TODO make it free)
+        :param filter_size: the filter size of the layer to be analyzed (has to be quadratic TODO make it free)
+        '''
+        super(ArgmaxPositions, self).__init__(**kwargs)
+        self.stride = stride
+        self.filter_size = filter_size
+        self.layer_output = layer_output
+        self.layer_weights = layer_weights
+
+        # reference value for later
+        self.zero = tf.constant(0, dtype=tf.float32)
+
+    def build(self, input_shape):
+        super(ArgmaxPositions, self).build(input_shape)  # Be sure to call this at the end
+
+    def update_relevance(self, relevance_index):
+        '''
+        finds the most contributing position for a given relevance value and propagates the relevance to this position
+        :param relevance_index: index of the relevance value to be analyzed
+        :return: a mask with only one non-zero entry (with value equal to the relevance value to be analyzed)
+                at the most contributing position. The mask has the same shape as the layer_output.
+        '''
+        # get x and y range of the relevant part of the picture
+        x_start = self.stride * relevance_index[1]
+        x_end = self.stride * relevance_index[1] + self.filter_size
+        y_start = self.stride * relevance_index[2]
+        y_end = self.stride * relevance_index[2] + self.filter_size
+
+        # getting correct input in the batch
+        batch_input = self.layer_output[relevance_index[0]]
+
+        # getting the part/window of the input which corresponds to the relevance value
+        input_patch = batch_input[x_start:x_end, y_start:y_end, :]
+
+        # getting the filter which corresponds to the relevance value
+        weight = self.layer_weights[:, :, :, relevance_index[3]]
+
+        # callculating pointwise product, which is also used during forward propagation
+        product = input_patch * weight
+
+        # Getting local position of the most contributing neuron in this window
+        old_shape = product.shape
+        product = keras.layers.Flatten()(product)
+        product = tf.expand_dims(product, axis=0)
+        product = keras.layers.Flatten()(product)
+        position = tf.argmax(product, axis=-1)[0]
+        position = tf.unravel_index(position, old_shape)
+
+        # getting global version of the local position
+        global_position = [position[0] + x_start, position[1] + y_start, position[2]]
+
+        # ravel gloabl position, indexes are shifted since out.shape has an empty first dimension
+        global_index = global_position[0] * self.layer_output.shape[-2] * self.layer_output.shape[-1] + global_position[1] * self.layer_output.shape[-1] + \
+                       global_position[2]
+
+        # generate one_hot mask with the relevance value at the global argamx position
+        mask = K.one_hot(global_index, tf.reduce_prod(batch_input.shape))
+        relevance_value = self.relevance_values[0, relevance_index[1], relevance_index[2], relevance_index[3]]
+        mask = mask * relevance_value
+        mask = K.reshape(mask, batch_input.shape)
+
+        return mask
+
+    def call(self, inputs):
+        '''
+            Caluclates the relvance vlalues of the analyzed layer according to the argmax-rule.
+            Assumes no padding.
+            :param inputs: the relevance values of the layer succeeding the layer to be analyzed
+            :returns: an array containing the relvance vlalues of the analyzed layer according to the argmax-rule
+        '''
+        # get all non-zero relevance values
+        self.relevance_values = inputs
+        where = tf.not_equal(inputs, self.zero)
+        indices = tf.where(where)
+
+        # generate a mask for each relevance value, which only has this relevance value as non-zero value
+        # at the most contributing position
+        relevance_masks = tf.map_fn(self.update_relevance, indices, dtype=tf.float32)
+
+        #sum over all masks
+        new_relevance_array = tf.reduce_sum(relevance_masks, axis=0)
+
+        #this is needed for shaping reasons TODO innvestigate further
+        new_relevance_array = K.expand_dims(new_relevance_array, axis=0)
+
+        return new_relevance_array
