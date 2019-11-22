Added video generation into stream generator. Fixed model decisions/actions by scaling the input between 0-1. Q-values are still not exactly the same for tensorflow and keras but the argmax is always the same.

stream_generator.py
@@ -29,10 +29,9 @@ def save_q_values(array, save_file, frame):
 #model = keras.models.load_model('models/MsPacman_1M_reward36komma3_action_only.h5')
 model = keras.models.load_model('models/MsPacman_2M_2_reward47komma5_action_only.h5')
 
-
 model.summary()
 
-#analyzer_z = innvestigate.analyzer.LRPAlpha1Beta0IgnoreBias(model)
+analyzer_z = innvestigate.analyzer.LRPAlpha1Beta0IgnoreBias(model)
 analyzer_arg = Argmax(model)
 
 env = gym.make('MsPacmanNoFrameskip-v4')
@@ -50,7 +49,7 @@ if __name__ == '__main__':
         if _ < 4:
             action = env.action_space.sample()
         else:
-            my_input = np.expand_dims(stacked_frames, axis=0)
+            my_input = np.expand_dims(stacked_frames, axis=0) / 255
             output = model.predict(my_input)
             action = np.argmax(np.squeeze(output))
 
@@ -60,9 +59,9 @@ if __name__ == '__main__':
             argmax = image_utils.normalise_image(argmax)
 
             # for future work
-            # z_rule = analyzer_z.analyze(my_input)
-            # z_rule = np.squeeze(z_rule)
-            # z_rule = image_utils.normalise_image(z_rule)
+            z_rule = analyzer_z.analyze(my_input)
+            z_rule = np.squeeze(z_rule)
+            z_rule = image_utils.normalise_image(z_rule)
 
             save_array(my_input,save_file_state, _)
             image = np.squeeze(my_input)
@@ -80,12 +79,13 @@ if __name__ == '__main__':
                 save_frame(saliency, save_file_argmax, index)
 
                 # # for future work
-                # saliency = image_utils.add_saliency_to_image(z_rule[:, :, 3], observation, 2)
-                # save_frame(saliency, save_file_z, index)
+                saliency = image_utils.add_saliency_to_image(z_rule[:, :, 3], observation, 2)
+                save_frame(saliency, save_file_z, index)
 
 
 
         stacked_frames, observations, reward, done, info = wrapper.step(action)
         env.render()
 
-
+image_utils.generate_video('stream/argmax/','stream/','argmax.mp4')
+image_utils.generate_video('stream/screen/','stream/','screen.mp4')