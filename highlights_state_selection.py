968268fbef686a14f1bed5f3d0ffafe4321a8bb0
ofraam <oamir@seas.harvard.edu>
2019-12-10 17:30:43 +0200

    new highlights summary implementation
    takes into account the context around an important state

--- highlights_state_selection.py
+++ highlights_state_selection.py
@@ -1,11 +1,49 @@
 import numpy as np
 import pandas as pd
 import os
+from bisect import bisect
+from bisect import insort_left
 
 
-def highlights(state_importance_df, budget, context_length):
-    top_states = state_importance_df.nlargest(budget, 'importance')['state'].values
-    return top_states
+def highlights(state_importance_df, budget, context_length, minimum_gap):
+    ''' generate highlights summary
+    :param state_importance_df: dataframe with 2 columns: state and importance score of the state
+    :param budget: allowed length of summary - note this includes only the important states, it doesn't count context
+    around them
+    :param context_length: how many states to show around the chosen important state (e.g., if context_lenght=10, we
+    will show 10 states before and 10 states after the important state
+    :param minimum_gap: how many states should we skip after showing the context for an important state. For example, if
+    we chose state 200, and the context length is 10, we will show states 189-211. If minimum_gap=10, we will not
+    consider states 212-222 and states 178-198 because they are too close
+    :return: a list with the indices of the important states. Note, it currently doesn't return the indices for the
+    context around them, we can add that here or handle it when we create the visual summary
+    '''
+    sorted_df = state_importance_df.sort_values(['importance'], ascending=False)
+    summary_states = []
+    for index, row in sorted_df.iterrows():
+
+        state_index = row['state']
+        index_in_summary = bisect(summary_states, state_index)
+        print('state: ', state_index)
+        print('index in summary: ', index_in_summary)
+        print('summary: ', summary_states)
+        state_before = None
+        state_after = None
+        if index_in_summary > 0:
+            state_before = summary_states[index_in_summary-1]
+        if index_in_summary < len(summary_states):
+            state_after = summary_states[index_in_summary]
+        if state_after is not None:
+            if state_index+context_length+minimum_gap > state_after:
+                continue
+        if state_before is not None:
+            if state_index-context_length-minimum_gap < state_before:
+                continue
+        insort_left(summary_states,state_index)
+        if len(summary_states) == budget:
+            break
+
+    return summary_states
 
 
 def compute_states_importance(states_q_values_df):
@@ -33,8 +71,13 @@ def read_q_value_files(path):
     return q_values_df
 
 if __name__ == '__main__':
-    test_data = pd.DataFrame({'state':[1,2,3],'q_values':[[1,2,3],[1,1,1],[2,1,1]]})
-    # print(highlights(test_data,2))
-    q_values_df = read_q_value_files('q_values')
-    states_q_values_df = compute_states_importance(q_values_df)
-    states_q_values_df.to_csv('states_importance.csv')
+    # test_data = pd.DataFrame({'state':[1,2,3],'q_values':[[1,2,3],[1,1,1],[2,1,1]]})
+    # # print(highlights(test_data,2))
+    # q_values_df = read_q_value_files('q_values')
+    # states_q_values_df = compute_states_importance(q_values_df)
+    # print(highlights(highlights,20,10,10))
+    # states_q_values_df.to_csv('states_importance.csv')
+    states_q_values_df = pd.read_csv('states_importance.csv')
+    print(highlights(states_q_values_df,20,10,10))
+    # a = [1,4,6,10]
+    # print(bisect(a,7))
