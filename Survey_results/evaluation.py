7661c732f9a894e3fa9ae08e94dedc92f482d109
TobiasHuber <tobiashuber94@gmail.com>
2020-02-25 18:31:28 +0100

    improved evaluation.py

--- Survey_results/evaluation.py
+++ Survey_results/evaluation.py
@@ -5,31 +5,52 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 import os
 
-
-
 def rank_biserial_effect_size(x,y):
     mann_whitney_res = stats.mannwhitneyu(x, y)
     u = mann_whitney_res[0]
     effect_size = 1.0-((2.0*u)/(len(x)*len(y)))
     return effect_size
 
-def test(data, column_name):
+def mann_whitney(data, column_name):
+    ''' doing the mann whitney test for the dependant variable in the given column
+    '''
     data_random = data.loc[data.randnumber==1]
     data_randomLRP = data.loc[data.randnumber==3]
     data_highlights = data.loc[data.randnumber==2]
     data_highlightsLRP = data.loc[data.randnumber==4]
 
-    stats.mannwhitneyu(data_random.percentTrustCorrect.values, data_highlights.percentTrustCorrect.values, alternative='less')
-    rank_biserial_effect_size(data_random.percentTrustCorrect.values, data_highlights.percentTrustCorrect.values)
-    rank_biserial_effect_size(data_highlights.percentTrustCorrect.values, data_random.percentTrustCorrect.values)
+    random_values = data_random[column_name].values
+    randomLRP_values = data_randomLRP[column_name].values
+    highlights_values = data_highlights[column_name].values
+    highlightsLRP_values = data_highlightsLRP[column_name].values
+
+    print('##### testing the column:', column_name,'#####')
+
+    print('random < randomLRP')
+    print(stats.mannwhitneyu(random_values, randomLRP_values, alternative='less'))
+    print(rank_biserial_effect_size(random_values, randomLRP_values))
+    print('highlights < highlightsLRP')
+    print(stats.mannwhitneyu(highlights_values, highlightsLRP_values, alternative='less'))
+    print(rank_biserial_effect_size(highlights_values, highlightsLRP_values))
+    print('random < highlights')
+    print(stats.mannwhitneyu(random_values, highlights_values, alternative='less'))
+    print(rank_biserial_effect_size(random_values, highlights_values))
+    print('randomLRP < highlightsLRP')
+    print(stats.mannwhitneyu(randomLRP_values, highlightsLRP_values, alternative='less'))
+    print(rank_biserial_effect_size(randomLRP_values, highlightsLRP_values))
 
 def show_and_save_plt(ax ,file_name, y_label=None, title = None,ylim =None):
     if y_label != None:
         plt.ylabel(y_label)
     #plt.supitle=(title)t
-    ax.set(title=title)
+    #ax.set(title=title)
     if ylim != None:
         ax.set(ylim=ylim)
+
+    # ax.set_ylabel('Entropy (solvers)', fontsize=14)
+    # ax.set_xlabel('Condition', fontsize=14)
+    ax.yaxis.label.set_size(14)
+    ax.xaxis.label.set_size(14)
 
     file_name = os.path.join('figures', file_name)
     if not (os.path.isdir(file_name)):
@@ -70,71 +91,25 @@ def satisfaction_analysis(number, data):
 
     data[result_name] = avg_satisfaction
     # +data['explSatisfaction1[3]_rev']+data['explSatisfaction1[4]']+data['explSatisfaction1[5]']+data['explSatisfaction1[6]']
-    ax = sns.barplot(x='condition', y=result_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.barplot(x='condition', y=result_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, result_name, y_label='Average user rating', title=title, ylim=(1, 7))
 
-def analyze_demographic(data):
-    # 1: <17, 2: 18-24, 3:25-34, 4:35-44, 5:45-54, 6:55-64, 7 >65, 8 not specify
-    ages = data.age.values
-    ages = ages[np.where(ages != 8)]
-    print('age mean:', np.mean(ages))
-    print('age median:', np.median(ages))
+def analyze_distribution(data, columns):
+    df = data[columns]
+    def get_column_number(column):
+        if column in columns:
+            return columns.index(column) + 1
+        else:
+            return column
 
-    # 1: male, 2: female, 3:prefer not to answer
-    genders = data.gender.values
-    genders = genders[np.where(genders != 3)]
-    genders = genders[np.where(genders != 4)]
-    genders = genders - 1
-    print('number females:', genders.sum())
+    df = pd.melt(df)
+    df.variable =df.variable.apply(get_column_number)
+    ax = sns.barplot(x="variable", y="value", data=df)
 
-    # 1: never played, 2: <1year, 3: <5years, 4: >5years ago
-    pacman_experience = data.experiencePacman.values
-    print('pacman exp median:', np.median(pacman_experience))
-    print('pacman exp mean:', np.mean(pacman_experience))
-
-    AI_exp = data.experienceAI.values
-    print('number of people with Ai experience:', np.sum(AI_exp))
-
-    attitude = data['outcomeAI[1]'].values
-    attitude = attitude[np.where(attitude != 6)]
-    print('mean attitude towards AI', np.mean(attitude))
-
-    data = data.loc[AI_exp == 1]
-
-    media = data['experienceAI2[1]'].values
-    print('I know AI from the media:', np.sum(media))
-    technology = data['experienceAI2[2]'].values
-    print("technology:", np.sum(technology))
-    technology_work = data['experienceAI2[3]'].values
-    print('technology work:', np.sum(technology_work))
-    course = data['experienceAI2[4]'].values
-    print('AI related course', np.sum(course))
-    research = data['experienceAI2[5]'].values
-    print('research on AI', np.sum(research))
-
-#####  .... questions
-
-data = pd.read_csv('fusion_trust_eval.csv')
-analyze_demographic(data)
-
-data_random = data.loc[data.randnumber==1]
-print('##########Condition 1##########')
-analyze_demographic(data_random)
-
-data_highlights = data.loc[data.randnumber==3]
-print('##########Condition 2##########')
-analyze_demographic(data_highlights)
-
-data_randomLRP = data.loc[data.randnumber==2]
-print('##########Condition 3##########')
-analyze_demographic(data_randomLRP)
-
-data_highlightsLRP = data.loc[data.randnumber==4]
-print('##########Condition 4##########')
-analyze_demographic(data_highlightsLRP)
+    return ax
 
 #####Analysis of trust task #####
-data = pd.read_csv('fusion_trust_eval.csv')
+data = pd.read_csv('fusion_final.csv')
 
 data['TrustClicksTotal'] = data.videoClicks1B + data.videoClicks1A + data.videoClicks2A + data.videoClicks2B + \
                            data.videoClicks3A + data.videoClicks3B
@@ -148,44 +123,50 @@ print('did not watch:', difference)
 
 data['numTrustCorrect'] = data.trust1correct + data.trust2correct + data.trust3correct
 data['percentTrustCorrect'] = data.numTrustCorrect.apply(lambda x: x/3.0)
-data['condition'] = data.randnumber.apply(lambda x: 'R' if x==1 else 'HL' if x==2 else 'R-S' if x==3 else 'HL-S')
+data['condition'] = data.randnumber.apply(lambda x: 'R' if x==1 else 'H' if x==2 else 'R+S' if x==3 else 'H+S')
 data.head()
 
 
 for i in range(1,4):
     column_name = 'trust'+ str(i) +'correct'
     title = 'comparison task for agent ' + str(i)
-    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents',column_name), 'percentage of correct selections', title=title)
 
     column_name = 'trustConfidence' + str(i) + '[confidence1]'
-    ax = sns.boxplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.boxplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents',column_name), ylim=(1, 7))
 
     column_name = 'videoClicks' + str(i)
     data[column_name] = data[column_name+'A'] + data[column_name+'B']
     title = 'pauses during the comparison task for agent ' + str(i)
-    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents', column_name), 'number of pauses', title=title, ylim=(0, 10))
 
 
-ax = sns.boxplot(x='condition', y='percentTrustCorrect', data=data, order=['R','HL','R-S','HL-S'])
-show_and_save_plt(ax,'percentTrustCorrect','percentage of correct selections', 'combined trust tasks')
+ax = sns.barplot(x='condition', y='percentTrustCorrect', data=data, order=['R','H','R+S','H+S'])
+show_and_save_plt(ax,'TrustPercentCorrect','percentage of correct selections', 'combined trust tasks')
 
 satisfaction_analysis(2, data)
 
-data['trustConfidenceAvg'] = (data['trustConfidence2[confidence1]']+data['trustConfidence3[confidence1]']+data['trustConfidence1[confidence1]'])/3.0
-ax = sns.boxplot(x='condition', y='trustConfidenceAvg', data=data, order=['R','HL','R-S','HL-S'])
-show_and_save_plt(ax,'trustConfidenceAvg',ylim=(1,7))
+
+column_name = 'TrustConfAvg'
+data[column_name] = (data['trustConfidence2[confidence1]']+data['trustConfidence3[confidence1]']+data['trustConfidence1[confidence1]'])/3.0
+ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
+show_and_save_plt(ax, column_name, y_label='average confidence',
+                  title='Average confidence in the agent selection',ylim=(1,7))
 
 data['trustTimeAvg'] = (data['trust1Time']+data['trust2Time']+data['trust3Time'])/3.0
-ax = sns.boxplot(x='condition', y='trustTimeAvg', data=data, order=['R','HL','R-S','HL-S'])
+ax = sns.boxplot(x='condition', y='trustTimeAvg', data=data, order=['R','H','R+S','H+S'])
 show_and_save_plt(ax, 'trustTimeAvg', ylim=(0, 500))
 
 data['TrustClicksAvg'] = data['TrustClicksTotal'] / 3
-ax = sns.barplot(x='condition', y='TrustClicksAvg', data=data, order=['R','HL','R-S','HL-S'])
+ax = sns.boxplot(x='condition', y='TrustClicksAvg', data=data, order=['R','H','R+S','H+S'])
 show_and_save_plt(ax, 'TrustClicksAvg', y_label='average number of clicks', title= 'Pauses of the Video in the trust task', ylim=(0, 10))
 
+
+mann_whitney(data, 'percentTrustCorrect')
+mann_whitney(data, 'explSatisfaction' + 'Trust' + 'Avg')
 
 
 
@@ -193,7 +174,7 @@ show_and_save_plt(ax, 'TrustClicksAvg', y_label='average number of clicks', titl
 
 
 ############ Analysis of Retrospection task ########
-data = pd.read_csv('fusion_trust_eval.csv')
+data = pd.read_csv('fusion_final.csv')
 
 data['retroClicksTotal'] = data.retrospectionClicks1 + data.retrospectionClicks2 + data.retrospectionClicks3
 
@@ -204,7 +185,7 @@ new_number = len(data['retroClicksTotal'])
 difference = total_number - new_number
 print('did not watch Retrospection:', difference)
 
-data['condition'] = data.randnumber.apply(lambda x: 'R' if x==1 else 'HL' if x==2 else 'R-S' if x==3 else 'HL-S')
+data['condition'] = data.randnumber.apply(lambda x: 'R' if x==1 else 'H' if x==2 else 'R+S' if x==3 else 'H+S')
 data.head()
 
 
@@ -217,39 +198,39 @@ data['retroClicksTotal'] = data['retroClicksTotal'] / 3
 
 for i in range(1,4):
     column_name = 'retrospection' + str(i) + 'Points'
-    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents',column_name), ylim=(0, 3))
 
     column_name = 'retrospection' + str(i) + 'Goal'
-    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents',column_name), ylim=(0, 3))
 
     column_name = 'retrospection' + str(i) + '[1]'
-    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents','retrospectionPacman'+str(i)), ylim=(0, 3))
 
     column_name = 'retrospectionClicks' + str(i)
-    ax = sns.boxplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.boxplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents',column_name), ylim=(0, 10))
 
 
 
-ax = sns.barplot(x='condition', y='retroScoreTotal', data=data, order=['R','HL','R-S','HL-S'])
+ax = sns.barplot(x='condition', y='retroScoreTotal', data=data, order=['R','H','R+S','H+S'])
 show_and_save_plt(ax,'retroScoreTotal',ylim=(0,3))
 
-ax = sns.barplot(x='condition', y='retroGoalTotal', data=data, order=['R','HL','R-S','HL-S'])
+ax = sns.barplot(x='condition', y='retroGoalTotal', data=data, order=['R','H','R+S','H+S'])
 show_and_save_plt(ax,'retroGoalTotal',ylim=(0,3))
 
-ax = sns.barplot(x='condition', y='retroPacmanTotal', data=data, order=['R','HL','R-S','HL-S'])
+ax = sns.barplot(x='condition', y='retroPacmanTotal', data=data, order=['R','H','R+S','H+S'])
 show_and_save_plt(ax,'retroPacmanTotal',ylim=(0,3))
 
-ax = sns.barplot(x='condition', y='retroClicksTotal', data=data, order=['R','HL','R-S','HL-S'])
+ax = sns.boxplot(x='condition', y='retroClicksTotal', data=data, order=['R','H','R+S','H+S'])
 show_and_save_plt(ax,'retroClicksAvg',y_label='average number of clicks', title='Pauses of the Video in the analysis task', ylim=(0, 10))
 
 avg_conf = None
 for i in range(1,4):
     column_name = 'RetrospectionConf' + str(i) + '[predictConf1]'
-    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+    ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
     show_and_save_plt(ax, os.path.join('single_agents',column_name), y_label='confidence',ylim=(1,7),
                       title='Confidence in the analysis of agent'+ str(i))
 
@@ -261,13 +242,15 @@ for i in range(1,4):
 
 column_name = 'retroConfAvg'
 data[column_name] = avg_conf / 3
-ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'HL', 'R-S', 'HL-S'])
+ax = sns.barplot(x='condition', y=column_name, data=data, order=['R', 'H', 'R+S', 'H+S'])
 show_and_save_plt(ax, column_name, y_label='average confidence',
                   title='Average confidence in the analysis',ylim=(1,7))
 
 data['retroTimeAvg'] = (data['retro1Time']+data['retro2Time']+data['retro3Time'])/3.0
-ax = sns.barplot(x='condition', y='retroTimeAvg', data=data, order=['R','HL','R-S','HL-S'])
-show_and_save_plt(ax, 'trustTimeAvg', ylim=(0, 500))
+ax = sns.boxplot(x='condition', y='retroTimeAvg', data=data, order=['R','H','R+S','H+S'])
+show_and_save_plt(ax, 'retroTimeAvg', ylim=(0, 500))
 
 satisfaction_analysis(1,data)
 
+mann_whitney(data, 'retroScoreTotal')
+mann_whitney(data, 'explSatisfaction' + 'Retro' + 'Avg')
---
+++ Survey_results/evaluation_demographic.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import os
+
+def show_and_save_plt(ax ,file_name, y_label=None, title = None,ylim =None):
+    if y_label != None:
+        plt.ylabel(y_label)
+    #plt.supitle=(title)t
+    ax.set(title=title)
+    if ylim != None:
+        ax.set(ylim=ylim)
+
+    file_name = os.path.join('figures', file_name)
+    if not (os.path.isdir(file_name)):
+        os.makedirs(file_name)
+        os.rmdir(file_name)
+    plt.savefig(file_name)
+
+    plt.show()
+
+def analyze_demographic(data):
+
+    ax = analyze_distribution(data, ['experienceAI2[1]', 'experienceAI2[2]', 'experienceAI2[3]', 'experienceAI2[4]',
+                                'experienceAI2[5]'])
+    return ax
+
+def analyze_distribution(data, columns):
+    df = data[columns]
+    def get_column_number(column):
+        if column in columns:
+            return columns.index(column) + 1
+        else:
+            return column
+
+    df = pd.melt(df)
+    df.variable =df.variable.apply(get_column_number)
+    ax = sns.barplot(x="variable", y="value", data=df)
+
+    return ax
+
+#####  .... questions
+
+data = pd.read_csv('fusion_final.csv')
+data['condition'] = data.randnumber.apply(lambda x: 'R' if x==1 else 'H' if x==2 else 'R+S' if x==3 else 'H+S')
+data.head()
+
+# 1: <17, 2: 18-24, 3:25-34, 4:35-44, 5:45-54, 6:55-64, 7 >65, 8 not specify
+ages = data.age.values
+ages = ages[np.where(ages != 8)]
+print('age median:', np.median(ages))
+
+data_ages = data.loc[data.age != 1]
+ax = sns.boxplot(x='condition', y='age', data=data_ages, order=['R', 'H', 'R+S', 'H+S'])
+show_and_save_plt(ax, 'demogrpahic/ages', y_label='age group', ylim=(1,7))
+
+
+# 1: male, 2: female, 3:prefer not to answer
+genders = data.gender.values
+genders = genders[np.where(genders != 3)]
+genders = genders[np.where(genders != 4)]
+genders = genders - 1
+print('number females:', genders.sum())
+
+data_gender = data.loc[data.gender < 3]
+#get males to 0 and females to 1
+data_gender.gender = data_gender.gender.values - 1
+ax = sns.barplot(x='condition', y='gender', data=data_gender, order=['R', 'H', 'R+S', 'H+S'])
+show_and_save_plt(ax, 'demogrpahic/number_females', y_label='percentage of female participants')
+
+# 1: never played, 2: <1year, 3: <5years, 4: >5years ago
+#reorder such that it goes up with experience
+data['experiencePacman'] = data.experiencePacman.apply(lambda x: 2 if x == 4 else 4 if x == 2 else x)
+pacman_experience = data['experiencePacman'].values
+print('pacman exp median:', np.median(pacman_experience))
+print('pacman exp mean:', np.mean(pacman_experience))
+ax = sns.boxplot(x='condition', y='experiencePacman', data=data, order=['R', 'H', 'R+S', 'H+S'])
+show_and_save_plt(ax, 'demogrpahic/pacman_experience', y_label='Pacman experience')
+
+
+attitude = data['outcomeAI[1]'].values
+attitude = attitude[np.where(attitude != 6)]
+print('mean attitude towards AI', np.mean(attitude))
+
+data_attiude = data.loc[data['outcomeAI[1]'] != 6]
+ax = sns.barplot(x='condition', y='outcomeAI[1]', data=data_attiude, order=['R', 'H', 'R+S', 'H+S'])
+show_and_save_plt(ax, 'demogrpahic/Attitude_towards_AI', y_label='Attitude towards AI')
+
+
+AI_exp = data.experienceAI.values
+print('number of people with Ai experience:', np.sum(AI_exp))
+
+data_random = data.loc[data.randnumber==1]
+print('##########Condition 1##########')
+ax = analyze_demographic(data_random)
+show_and_save_plt(ax, 'demogrpahic/AiExperience_random', y_label='percent of participants', ylim=[0,1])
+
+data_highlights = data.loc[data.randnumber==2]
+print('##########Condition 2##########')
+analyze_demographic(data_highlights)
+show_and_save_plt(ax, 'demogrpahic/AiExperience_highlights', y_label='percent of participants', ylim=[0,1])
+
+data_randomLRP = data.loc[data.randnumber==2]
+print('##########Condition 3##########')
+analyze_demographic(data_randomLRP)
+show_and_save_plt(ax, 'demogrpahic/AiExperience_randomLRP', y_label='percent of participants', ylim=[0,1])
+
+data_highlightsLRP = data.loc[data.randnumber==4]
+print('##########Condition 4##########')
+analyze_demographic(data_highlightsLRP)
+show_and_save_plt(ax, 'demogrpahic/AiExperience_highlightsLRP', y_label='percent of participants', ylim=[0,1])
+
+
+data = data.loc[data.experienceAI == 1]
+media = data['experienceAI2[1]'].values
+print('I know AI from the media:', np.sum(media))
+technology = data['experienceAI2[2]'].values
+print("technology:", np.sum(technology))
+technology_work = data['experienceAI2[3]'].values
+print('technology work:', np.sum(technology_work))
+course = data['experienceAI2[4]'].values
+print('AI related course', np.sum(course))
+research = data['experienceAI2[5]'].values
+print('research on AI', np.sum(research))
